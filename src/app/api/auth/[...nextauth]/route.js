196fddd5767b0d725168d4cbc0e01ac8e788b113
ClintJoey19 <llosalaclintjoey@gmail.com>
2024-03-26 17:59:58 +1100

    fix the authentication

--- src/app/api/auth/[...nextauth]/route.js
+++ src/app/api/auth/[...nextauth]/route.js
@@ -7,23 +7,22 @@ const GOOGLE_ID = process.env.CLIENT_ID
 const GOOGLE_SECRET = process.env.CLIENT_SECRET
 
 const authOptions = {
+  session: {
+    strategy: 'jwt'
+  },
     providers: [
         GoogleProvider({
             clientId: GOOGLE_ID,
             clientSecret: GOOGLE_SECRET,
+            authorizationUrl: "https://accounts.google.com/o/oauth2/v2/auth?hd=aretex.com.au",
             authorization: {
                 params: {
                   prompt: "consent",
                   access_type: "offline",
-                  response_type: "code"
+                  response_type: "code",
+                  scope: "openid email profile https://www.googleapis.com/auth/drive https://www.googleapis.com/auth/drive.file"
                 }
               },
-              scope: [
-                "https://www.googleapis.com/auth/userinfo.profile",
-                "https://www.googleapis.com/auth/drive",
-                "https://www.googleapis.com/auth/drive.file"
-              ],
-              include_granted_scopes: true
         })
     ],
     callbacks: {
@@ -49,6 +48,18 @@ const authOptions = {
           }
         }
         return true
+      },
+      async jwt({ token, account }) {
+        if (account) {
+          token.accessToken = account.access_token
+          token.refreshToken = account.refresh_token
+        }
+        return token
+      },
+      async session({ session, token }) {
+        session.accessToken = token.accessToken
+        session.refreshToken = token.refreshToken
+        return session
       }
     }
 }
