d6e8bdf4931a0386c3bc538c7052b450798e046f
ClintJoey19 <llosalaclintjoey@gmail.com>
2024-04-05 18:05:55 +1100

    chore: fix serverside fetching of drive

--- src/app/dashboard/shared-drives/page.jsx
+++ src/app/dashboard/shared-drives/page.jsx
@@ -1,6 +1,15 @@
 import { DataTableDemo } from "@/components/dashboard/DataTable";
 
+async function getDrives() {
+  const res = await fetch("http://localhost:3000/api/dashboard/shared-drive");
+
+  return res;
+}
+
 const SharedDrives = async () => {
+  const drives = await getDrives();
+  const data = await drives.json();
+  console.log(data);
   return (
     <main className="m-2 bg-white rounded-md border">
       <div className="p-4 flex flex-col gap-4">
